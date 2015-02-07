4f01b3c57a327b2386453ac14d156c73e63c1b26
20 <20@I20>
2015-02-07 09:51:52 -0500

    http://www.tutorialspoint.com/java/java_decision_making.htm

---
+++ src/ciclos/Ciclos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclos;
+
+/**
+ *
+ * @author 20
+ */
+import java.io.*;
+
+public class Ciclos {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String args[]){
+      int x = 330;
+
+      if( x == 10 ){
+         System.out.print("Value of X is 10");
+      }else if( x == 20 ){
+         System.out.print("Value of X is 20");
+      }else if( x == 30 ){
+         System.out.print("Value of X is 30");
+      }else{
+         System.out.print("This is else is none");
+      }
+   }
+}
+    
